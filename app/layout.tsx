ff4c5093f4d96562ceb4745b1fcfca1b32c1aad1
Sebastian Melendez <sebm@seas.upenn.edu>
2023-11-06 20:37:46 -0500

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 
-const roboto = Roboto({subsets: ['latin'],  weight: ['100', '300', '400', '700'],})
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700"],
+});
 
 export const metadata: Metadata = {
-  title: 'RESCAI',
-  description: 'The International Expert Consortium on the Regulation, Economics and Computer Science of AI (RECSAI) aims to provide a platform to facilitate such cross-disciplinary enquiries.',
-}
+  title: "RESCAI",
+  openGraph: {
+    images: "./favicon.ico",
+  },
+  description:
+    "The International Expert Consortium on the Regulation, Economics and Computer Science of AI (RECSAI) aims to provide a platform to facilitate such cross-disciplinary enquiries.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={roboto.className}>{children}</body>
     </html>
-  )
+  );
 }
